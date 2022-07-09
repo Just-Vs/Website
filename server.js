4aaa1ebf91d195a1536a886b0000c040b4964a06
Just-Vs <99630937+Just-Vs@users.noreply.github.com>
2022-07-09 20:34:56 +0100

    Creating pages for register login and home

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.set('view-engine', 'ejs')
+app.get('/', (req, res)=> {
+  res.render('index.ejs', {numb:'5'})
+})
+
+app.get('/login', (req, res)=> {
+  res.render('login.ejs', {numb:'5'})
+})
+
+app.get('/register', (req, res)=> {
+  res.render('register.ejs', {numb:'5'})
+})
+app.listen(3000)
