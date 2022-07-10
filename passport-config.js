99aa31085aa064f7bd1684f0eb514c016f3f62a0
Just-Vs <99630937+Just-Vs@users.noreply.github.com>
2022-07-10 21:17:33 +0100

    Authenticate user info

--- passport-config.js
+++ passport-config.js
@@ -1,10 +1,10 @@
-const LocalStrategy = require(' passport-local').Strategy
+const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcrypt')
 
 
 
-function initialize(passport, getUserByEmail){
-  const authenticateUser = (email, password, done) =>{
+function initialize(passport, getUserByEmail, getUserById){
+  const authenticateUser = async (email, password, done) =>{
     const user = getUserByEmail
     if(user==null){
       return done(null,flase, {message: 'No user with that email'})
@@ -22,11 +22,12 @@ function initialize(passport, getUserByEmail){
     }
 
     }
-  }
-  passport.use(new LocalStrategy({ usernameField: 'email'}),
-  authenticateUser)
-  passport.serializeUser((user, done) => {})
-  passport.deserializeUser((user, done) => {})
-}
 
+    passport.use(new LocalStrategy({ usernameField: 'email'},
+    authenticateUser))
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((user, done) => {
+      return done(null, getUserById(id))
+    })
+  }
 module.exports = initialize
